8ab33ff0c4c29be259c1401fd62e52734ab1d80d
Travis Wylie <traviswylie58@gmail.com>
2026-08-13 14:06:58 +1200

    navbar update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav className="sidebar p-3 border-end vh-100" style={{ minWidth: '200px' }}>
       <ul className="nav flex-column">
-        <li className="nav-item"><Link to="/" className="nav-link text-dark">Home</Link></li>
-        <li className="nav-item"><Link to="/experience" className="nav-link text-dark">Experience</Link></li>
-        <li className="nav-item"><Link to="/projects" className="nav-link text-dark">Projects</Link></li>
-        <li className="nav-item"><Link to="/contact" className="nav-link text-dark">Contact</Link></li>
+        <li className="nav-item">
+          <NavLink to="/" className="nav-link">Home</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/experience" className="nav-link">Experience</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/projects" className="nav-link">Projects</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/contact" className="nav-link">Contact</NavLink>
+        </li>
       </ul>
     </nav>
   );
