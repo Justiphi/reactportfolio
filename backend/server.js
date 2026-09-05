9fab3eb27f59fda16496da3d4d27e8a8d372efab
Travis Wylie <traviswylie58@gmail.com>
2026-09-05 16:41:16 +1200

    frontend environment variable fix

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const sql = require('mssql');
 require('dotenv').config();
 
 const app = express();
-app.use(cors({ origin: `${process.env.FRONTEND_HOST}:${FRONTEND_PORT}` }));
+app.use(cors({ origin: `${process.env.FRONTEND_HOST}:${process.env.FRONTEND_PORT}` }));
 app.use(express.json()); // Allows parsing JSON bodies
 
 // MSSQL Configuration Object
